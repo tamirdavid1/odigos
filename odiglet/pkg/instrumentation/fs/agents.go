42911d2af2d843273700e2e93113e0073a8fc3f3
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-07-29 16:44:25 +0300

    fix

--- odiglet/pkg/instrumentation/fs/agents.go
+++ odiglet/pkg/instrumentation/fs/agents.go
@@ -1,14 +1,5 @@
 package fs
 
-import (
-	"os"
-	"os/exec"
-	"path/filepath"
-	"syscall"
-
-	"github.com/odigos-io/odigos/odiglet/pkg/log"
-)
-
 const (
 	containerDir   = "/instrumentations"
 	hostDir        = "/var/odigos"
@@ -19,51 +10,51 @@ const (
 
 func CopyAgentsDirectoryToHost() error {
 
-	// remove the current content of /var/odigos
-	// as we want a fresh copy of instrumentation agents with no files leftover from previous odigos versions.
-	// we cannot remove /var/odigos itself: "unlinkat /var/odigos: device or resource busy"
-	// so we will just remove it's content
-	entries, err := os.ReadDir(hostDir)
-	if err != nil {
-		return err
-	}
-	for _, entry := range entries {
-		entryPath := filepath.Join(hostDir, entry.Name())
-		err := os.RemoveAll(entryPath)
-		if err != nil {
-			return err
-		}
-	}
+	// // remove the current content of /var/odigos
+	// // as we want a fresh copy of instrumentation agents with no files leftover from previous odigos versions.
+	// // we cannot remove /var/odigos itself: "unlinkat /var/odigos: device or resource busy"
+	// // so we will just remove it's content
+	// entries, err := os.ReadDir(hostDir)
+	// if err != nil {
+	// 	return err
+	// }
+	// for _, entry := range entries {
+	// 	entryPath := filepath.Join(hostDir, entry.Name())
+	// 	err := os.RemoveAll(entryPath)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// }
 
-	err = copyDirectories(containerDir, hostDir)
-	if err != nil {
-		log.Logger.Error(err, "Error copying instrumentation directory to host")
-		return err
-	}
+	// err = copyDirectories(containerDir, hostDir)
+	// if err != nil {
+	// 	log.Logger.Error(err, "Error copying instrumentation directory to host")
+	// 	return err
+	// }
 
-	// Check if the semanage command exists when running on RHEL/CoreOS
-	_, err = exec.LookPath(filepath.Join(chrootDir, semanagePath))
-	if err == nil {
-		// Run the semanage command to add the new directory to the container_ro_file_t context
-		cmd := exec.Command(semanagePath, "fcontext", "-a", "-t", "container_ro_file_t", "/var/odigos(/.*)?")
-		syscall.Chroot(chrootDir)
-		err = cmd.Run()
-		if err != nil {
-			log.Logger.Error(err, "Error running semanage command")
-		}
-	}
+	// // Check if the semanage command exists when running on RHEL/CoreOS
+	// _, err = exec.LookPath(filepath.Join(chrootDir, semanagePath))
+	// if err == nil {
+	// 	// Run the semanage command to add the new directory to the container_ro_file_t context
+	// 	cmd := exec.Command(semanagePath, "fcontext", "-a", "-t", "container_ro_file_t", "/var/odigos(/.*)?")
+	// 	syscall.Chroot(chrootDir)
+	// 	err = cmd.Run()
+	// 	if err != nil {
+	// 		log.Logger.Error(err, "Error running semanage command")
+	// 	}
+	// }
 
-	// Check if the restorecon command exists when running on RHEL/CoreOS
-	_, err = exec.LookPath(filepath.Join(chrootDir, restoreconPath))
-	if err == nil {
-		// Run the restorecon command to apply the new context
-		cmd := exec.Command(restoreconPath, "-r", "/var/odigos")
-		syscall.Chroot(chrootDir)
-		err = cmd.Run()
-		if err != nil {
-			log.Logger.Error(err, "Error running restorecon command")
-		}
-	}
+	// // Check if the restorecon command exists when running on RHEL/CoreOS
+	// _, err = exec.LookPath(filepath.Join(chrootDir, restoreconPath))
+	// if err == nil {
+	// 	// Run the restorecon command to apply the new context
+	// 	cmd := exec.Command(restoreconPath, "-r", "/var/odigos")
+	// 	syscall.Chroot(chrootDir)
+	// 	err = cmd.Run()
+	// 	if err != nil {
+	// 		log.Logger.Error(err, "Error running restorecon command")
+	// 	}
+	// }
 
 	return nil
 }
