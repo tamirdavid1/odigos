0752298a7dc4638f80f712eeb69c0c1b2b15f414
Tamir David <tamirdavid@Tamirs-MacBook-Pro.local>
2024-08-13 13:45:47 +0300

    fix: not remove only the directories of the proceted files

--- odiglet/pkg/instrumentation/fs/remove.go
+++ odiglet/pkg/instrumentation/fs/remove.go
@@ -4,56 +4,49 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/odigos-io/odigos/odiglet/pkg/log"
 )
 
-func removeFilesInDir(hostDir string) error {
+func removeFilesInDir(hostDir string, filesToKeep map[string]struct{}) error {
 	shouldRecreateCFiles := ShouldRecreateAllCFiles()
 	log.Logger.V(0).Info(fmt.Sprintf("Removing files in directory: %s, shouldRecreateCFiles: %s", hostDir, fmt.Sprintf("%t", shouldRecreateCFiles)))
 
+	protectedDirs := make(map[string]bool)
+
+	if !shouldRecreateCFiles {
+		// Mark the directories containing the files as protected
+		for file := range filesToKeep {
+			dir := filepath.Dir(file)
+			for dir != hostDir {
+				protectedDirs[dir] = true
+				dir = filepath.Dir(dir)
+			}
+			protectedDirs[hostDir] = true // Protect the main directory
+		}
+	}
+
+	fmt.Printf("Protected dirs: %v\n", protectedDirs)
+
 	return filepath.Walk(hostDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
-			return err
+			return fmt.Errorf("error accessing path %s: %w", path, err)
 		}
 
-		// Skip the root directory itself]
-		if path == hostDir {
-			return nil
-		}
-
-		// Skip directories containing "ebpf" in their name, delete them in case of a full cleanup
-		if !shouldRecreateCFiles {
-			if info.IsDir() && strings.Contains(path, "ebpf") {
-				log.Logger.V(0).Info(fmt.Sprintf("Skipping removing directory: %s", path))
-				return nil
-			}
-		}
-
-		// Remove and skip other directories
 		if info.IsDir() {
+			if protectedDirs[path] {
+				log.Logger.V(0).Info(fmt.Sprintf("Skipping removing protected directory: %s", path))
+				return nil // Skip protected directories
+			}
+		}
+
+		// Remove unprotected files and directories
+		if !protectedDirs[path] {
+			log.Logger.V(0).Info(fmt.Sprintf("Removing file or directory: %s", path))
 			if err := os.RemoveAll(path); err != nil {
-				return fmt.Errorf("error removing directory %s: %w", path, err)
+				return fmt.Errorf("error removing %s: %w", path, err)
 			}
-			log.Logger.V(0).Info(fmt.Sprintf("Removed directory: %s", path))
-			return filepath.SkipDir
-		}
-
-		if !shouldRecreateCFiles {
-			// filter out C files in ebpf directories
-			if strings.Contains(filepath.Dir(path), "ebpf") {
-				switch ext := filepath.Ext(info.Name()); ext {
-				case ".so", ".node", "node.d", ".a":
-					return nil
-				}
-			}
-		}
-
-		// Remove the file
-		err = os.Remove(path)
-		if err != nil {
-			return err
+			return filepath.SkipDir // Skip further processing in removed directories
 		}
 
 		return nil
